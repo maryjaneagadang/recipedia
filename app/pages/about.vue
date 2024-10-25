39578aeb6018a379c13551289c8b1700d97d7473
maryjane.js <95892766+maryjaneagadang@users.noreply.github.com>
2024-10-25 12:01:15 +0800

    :sparkles: feat: start project

---
+++ app/pages/about.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>
+    <section class="container py-20">
+      <h1 class="text-4xl font-extrabold mb-6 text-balance">
+        About Recipedia
+      </h1>
+      <p class="text-xl mb-6 text-balance max-w-screen-lg">
+        Welcome to <span class="font-bold">Recipedia</span>, your one-stop destination for discovering,
+        creating, and sharing delicious recipes from around the world. Whether
+        you’re a seasoned chef or just starting your culinary journey, Recipedia
+        offers a platform to explore a vast collection of mouthwatering dishes,
+        learn new cooking techniques, and get inspired by a community of food
+        lovers.
+      </p>
+    </section>
+  </main>
+</template>
---
+++ app/pages/recipes/error.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps({
+  error: Object as () => NuxtError,
+});
+</script>
+
+<template>
+  <section class="flex min-h-screen flex-col items-center justify-center">
+    <h1 class="mb-2 text-9xl font-bold">
+      {{ error?.statusCode || 500 }}
+    </h1>
+    <p class="mb-10 text-xl sm:text-3xl">
+      {{ error?.statusMessage || "Something went wrong." }}
+    </p>
+    <NuxtLink
+      to="/"
+      class="px-4 py-2 text-white bg-dodgeroll-gold rounded-md text-lg cursor-pointer"
+    >
+      Go home
+    </NuxtLink>
+  </section>
+</template>
